c22b8e57a7519ca4f959a48f6f8cd719a6f5517c
Tolu Lawson <lawsontolu1@gmail.com>
2020-11-25 00:43:43 +0100

    Checkpoint

--- pages/_document.js
+++ pages/_document.js
@@ -24,13 +24,16 @@ class MyDocument extends Document {
         <meta property='og:url' content='https://tolulawson.com/' />
         <meta property='og:title' content='Tolu Lawson — Frontend Software Engineer' />
         <meta property='og:description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
-        <meta property='og:image' content='/share_preview.png' />
+        <meta property='og:image' content={`${typeof window !== 'undefined' ? window.loation.origin : ''}/share_preview.png`} />
 
         <meta property='twitter:card' content='summary_large_image' />
         <meta property='twitter:url' content='https://tolulawson.com/' />
         <meta property='twitter:title' content='Tolu Lawson — Frontend Software Engineer' />
         <meta property='twitter:description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
-        <meta property='twitter:image' content='/share_preview.png' />
+        <meta property='twitter:image' content={`${typeof window !== 'undefined' ? window.loation.origin : ''}/share_preview.png`} />
+
+        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
+        <link rel='icon' href='/favicon.ico' type='image/x-icon' />
         <body>
           <Main />
           <NextScript />
