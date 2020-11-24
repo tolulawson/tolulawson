59e401a5241ee625dffa1201808b60b03629ef84
Tolu Lawson <lawsontolu1@gmail.com>
2020-11-25 00:54:00 +0100

    -- Edit share sheet

--- pages/_document.js
+++ pages/_document.js
@@ -17,20 +17,22 @@ class MyDocument extends Document {
         <Head />
 
         <title>Tolu Lawson — Frontend Software Engineer</title>
-        <meta name='title' content='Tolu Lawson — Frontend Software Engineer' />
+        <meta name='title' content='Tolu Lawson — Software Engineer' />
         <meta name='description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
 
         <meta property='og:type' content='website' />
         <meta property='og:url' content='https://tolulawson.com/' />
-        <meta property='og:title' content='Tolu Lawson — Frontend Software Engineer' />
+        <meta property='og:title' content='Tolu Lawson — Software Engineer' />
         <meta property='og:description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
         <meta property='og:image' content={`${typeof window !== 'undefined' ? window.loation.origin : ''}/share_preview.png`} />
 
-        <meta property='twitter:card' content='summary_large_image' />
-        <meta property='twitter:url' content='https://tolulawson.com/' />
-        <meta property='twitter:title' content='Tolu Lawson — Frontend Software Engineer' />
-        <meta property='twitter:description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
-        <meta property='twitter:image' content={`${typeof window !== 'undefined' ? window.loation.origin : ''}/share_preview.png`} />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@tolulawson' />
+        <meta name='twitter:creator' content='@tolulawson' />
+        <meta property='og:url' content='https://tolulawson.com/' />
+        <meta property='og:title' content='Tolu Lawson — Software Engineer' />
+        <meta property='og:description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
+        <meta property='og:image' content={`${typeof window !== 'undefined' ? window.loation.origin : ''}/share_preview.png`} />
 
         <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
         <link rel='icon' href='/favicon.ico' type='image/x-icon' />
