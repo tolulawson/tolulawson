bb2fd7d1274fe6b2891610c62b4d26cd23cc6eaf
Tolu Lawson <lawsontolu1@gmail.com>
2020-11-26 16:15:22 +0100

    Checkpoint

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.scss';
+import '../styles/home.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
+import Link from 'next/link';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -13,30 +14,47 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
-        <Head />
+      <Html className='dark'>
+        <Head>
+          <meta name='title' content='Tolu Lawson — Software Engineer' />
+          <meta name='description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
 
-        <title>Tolu Lawson — Frontend Software Engineer</title>
-        <meta name='title' content='Tolu Lawson — Software Engineer' />
-        <meta name='description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
+          <meta property='og:type' content='website' />
+          <meta property='og:url' content='https://tolulawson.com/' />
+          <meta property='og:title' content='Tolu Lawson — Software Engineer' />
+          <meta property='og:description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
+          <meta property='og:image' content={`${typeof window !== 'undefined' ? window.loation.origin : ''}/share_preview.png`} />
 
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://tolulawson.com/' />
-        <meta property='og:title' content='Tolu Lawson — Software Engineer' />
-        <meta property='og:description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
-        <meta property='og:image' content={`${typeof window !== 'undefined' ? window.loation.origin : ''}/share_preview.png`} />
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta name='twitter:site' content='@tolulawson' />
+          <meta name='twitter:creator' content='@tolulawson' />
+          <meta property='og:url' content='https://tolulawson.com/' />
+          <meta property='og:title' content='Tolu Lawson — Software Engineer' />
+          <meta property='og:description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
+          <meta property='og:image' content={`${typeof window !== 'undefined' ? window.loation.origin : ''}/share_preview.png`} />
 
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:site' content='@tolulawson' />
-        <meta name='twitter:creator' content='@tolulawson' />
-        <meta property='og:url' content='https://tolulawson.com/' />
-        <meta property='og:title' content='Tolu Lawson — Software Engineer' />
-        <meta property='og:description' content='Software engineer with years of experience defining requirements, designing, implementing, testing, and delivering complex web applications and interactive experiences.' />
-        <meta property='og:image' content={`${typeof window !== 'undefined' ? window.loation.origin : ''}/share_preview.png`} />
-
-        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
-        <link rel='icon' href='/favicon.ico' type='image/x-icon' />
+          <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
+          <link rel='icon' href='/favicon.ico' type='image/x-icon' />
+        </Head>
         <body>
+          <script src='/js/setTheme.js' />
+          <nav>
+            <Link href='/'>
+              <a className='logo'>
+                <img src='logo_dark.svg' alt='logo' />
+              </a>
+            </Link>
+            <ul className='navigation'>
+              <li><Link href='/'>About</Link></li>
+              <li><Link href='/'>Projects</Link></li>
+              <li><Link href='/'>Articles</Link></li>
+              <li>
+                <button type='button'>
+                  <img src='dark_mode.svg' alt='dark-mode' />
+                </button>
+              </li>
+            </ul>
+          </nav>
           <Main />
           <NextScript />
         </body>
